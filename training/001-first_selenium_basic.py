16210057703c15655597e64e28ce4c0c2c318fb5
Juyong Hong <juyong3525@gmail.com>
2021-02-09 16:09:46 +0900

    Added first selenium basic

---
+++ training/001-first_selenium_basic.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+chromedriver = '/usr/local/Cellar/chromedriver/chromedriver'
+driver = webdriver.Chrome(chromedriver)
