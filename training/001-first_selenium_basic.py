4509b95c9a54b3074e5bf3aa397ff9bfa023c261
Juyong Hong <juyong3525@gmail.com>
2021-02-09 18:10:14 +0900

    Added headless chrome

--- training/001-first_selenium_basic.py
+++ training/001-first_selenium_basic.py
@@ -7,10 +7,27 @@ import time
 # driver = webdriver.Chrome(chromedriver)
 
 # phantomJS 사용
-phantomjs_file = '/usr/local/bin/phantomjs'
-driver = webdriver.PhantomJS(phantomjs_file)
+# phantomjs_file = '/usr/local/bin/phantomjs'
+# driver = webdriver.PhantomJS(phantomjs_file)
+
+# headless chrome 사용
+chromedriver = '/usr/local/Cellar/chromedriver/chromedriver'
+headless_options = webdriver.ChromeOptions()
+headless_options.add_argument('headless')
+headless_options.add_argument('window-size=1920x1080')
+headless_options.add_argument('disable-gpu')
+headless_options.add_argument(
+    'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36')
+headless_options.add_argument('lang=ko_KR')
+
+driver = webdriver.Chrome(chromedriver, options=headless_options)
+
 
 driver.get('http://www.python.org')
+print(driver.title)
+print(driver.current_url)
+print()
+
 assert "Python" in driver.title
 
 elem = driver.find_element_by_name("q")
@@ -22,8 +39,9 @@ time.sleep(2)
 
 assert "No results found." not in driver.page_source
 
-h3s = driver.find_elements_by_tag_name('h3')
-for h3 in h3s:
-    print(h3.text)
+data = driver.find_elements_by_css_selector(
+    "#content > div > section > form > ul > li > h3 > a")
+for item in data:
+    print(item.text)
 
 driver.quit()
