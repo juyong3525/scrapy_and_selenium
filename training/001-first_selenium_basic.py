f5d993a1081519d4cdfd1a27efc3a37b07ad7daf
Juyong Hong <juyong3525@gmail.com>
2021-02-09 16:38:42 +0900

    Added Web manipulation

--- training/001-first_selenium_basic.py
+++ training/001-first_selenium_basic.py
@@ -1,5 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 chromedriver = '/usr/local/Cellar/chromedriver/chromedriver'
 driver = webdriver.Chrome(chromedriver)
+
+driver.get('http://www.python.org')
+assert "Python" in driver.title
+
+elem = driver.find_element_by_name("q")
+elem.clear()
+
+elem.send_keys("python")
+elem.send_keys(Keys.RETURN)
+time.sleep(2)
+
+assert "No results found." not in driver.page_source
+
+h3s = driver.find_elements_by_tag_name('h3')
+for h3 in h3s:
+    print(h3.text)
+
+driver.quit()
