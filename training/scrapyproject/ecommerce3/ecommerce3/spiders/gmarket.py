38debe6fcdccfbf05e3d8c15477ca1100f355911
Juyong Hong <juyong3525@gmail.com>
2021-02-20 15:14:52 +0900

    Deleted test file

--- training/scrapyproject/ecommerce3/ecommerce3/spiders/gmarket.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-
-class GmarketSpider(scrapy.Spider):
-    name = 'gmarket'
-    start_urls = ['http://www.gmarket.co.kr/']
-
-    def parse(self, response):
-        print(response.text)
