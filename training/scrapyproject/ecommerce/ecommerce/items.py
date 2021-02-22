7944952000e4dc73976ceead307f21d25c4a4a40
Juyong Hong <juyong3525@gmail.com>
2021-02-22 16:36:14 +0900

    Learning how to use the scrapy pipeline part.2

--- training/scrapyproject/ecommerce/ecommerce/items.py
+++ training/scrapyproject/ecommerce/ecommerce/items.py
@@ -10,3 +10,4 @@ class EcommerceItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     title = scrapy.Field()
+    price = scrapy.Field()
--- training/scrapyproject/ecommerce/ecommerce/spiders/gmarket_best.py
+++ training/scrapyproject/ecommerce/ecommerce/spiders/gmarket_best.py
@@ -8,7 +8,13 @@ class GmarketBestSpider(scrapy.Spider):
     start_urls = ['http://corners.gmarket.co.kr/Bestsellers/']
 
     def parse(self, response):
-        titles = response.css('div.best-list > ul > li[id] > a::text').getall()
+        titles = response.css('div.best-list > ul > li > a::text').getall()
+        prices = response.css(
+            'div.best-list > ul > li > div.item_price > div.s-price > strong > span > span::text').getall()
 
         for num, title in enumerate(titles):
-            print(title)
+            doc = EcommerceItem()
+            doc['title'] = title
+            doc['price'] = prices[num].strip().replace(
+                '원', '').replace(',', '')
+            yield doc
