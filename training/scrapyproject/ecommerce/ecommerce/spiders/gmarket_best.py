966e7d5f0297c4b7ac2878de81bceec5be878db0
Juyong Hong <juyong3525@gmail.com>
2021-02-22 15:18:04 +0900

    Crawl the Gmarket with Scrapy

---
+++ training/scrapyproject/ecommerce/ecommerce/spiders/gmarket_best.py
@@ -0,0 +1,19 @@
+import scrapy
+from ecommerce.items import EcommerceItem
+
+
+class GmarketBestSpider(scrapy.Spider):
+    name = 'gmarket_best'
+    allowed_domains = ['corners.gmarket.co.kr/Bestsellers']
+    start_urls = ['http://corners.gmarket.co.kr/Bestsellers/']
+
+    def parse(self, response):
+        titles = response.css('div.best-list li > a::text').getall()
+        prices = response.css(
+            'div.best-list ul li div.item_price div.s-price strong span::text').getall()
+
+        for num, title in enumerate(titles):
+            doc = EcommerceItem()
+            doc['title'] = title
+            doc['price'] = prices[num]
+            yield doc
