24cc1f8e6fb172078ca5ee760f0f0aa8a30181d2
Juyong Hong <juyong3525@gmail.com>
2021-02-22 14:47:16 +0900

    Clean scrapy

--- training/scrapyproject/ecommerce2/ecommerce2/spiders/test2.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-
-class Test2Spider(scrapy.Spider):
-    name = 'test2'
-    start_urls = ['http://corners.gmarket.co.kr/Bestsellers/', 'http://promotion.gmarket.co.kr/Event/CouponZone.asp']
-
-    def parse(self, response):
-        print(response.url)
