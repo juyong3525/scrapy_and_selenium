116e024b08acf1276311dc06f3310aad2a49bb7d
Juyong Hong <juyong3525@gmail.com>
2021-02-24 14:45:00 +0900

    Gmarket crawling and learning how to use scrapy in practice part.2

--- training/scrapyproject/ecommerce/ecommerce/spiders/gmarket_category_all.py
+++ training/scrapyproject/ecommerce/ecommerce/spiders/gmarket_category_all.py
@@ -7,10 +7,13 @@ class GmarketCategoryAllSpider(scrapy.Spider):
     # __init__ 개념 (프로그램 실행 시 해당 메소드가 실행됨)
     def start_requests(self):
         yield scrapy.Request(url='http://corners.gmarket.co.kr/Bestsellers/', callback=self.parse_mainpages)
-        yield scrapy.Request(url='http://corners.gmarket.co.kr/Bestsellers?viewType=G&groupCode=G01', callback=self.parse_subcategories)
 
     def parse_mainpages(self, response):
         print("parse_mainpages")
-
-    def parse_subcategories(self, response):
-        print("parse_subcategories")
+        category_links = response.css(
+            'div.gbest-cate ul.by-group li a::attr("href")').getall()
+        category_names = response.css(
+            'div.gbest-cate ul.by-group li a::text').getall()
+        for index, category_link in enumerate(category_links):
+            print(
+                f'http://corners.gmarket.co.kr{category_link}', category_names[index])
