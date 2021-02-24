e6d4c24806c75ad3fc236d9f956e9b66a836a39a
Juyong Hong <juyong3525@gmail.com>
2021-02-24 15:40:02 +0900

    Gmarket crawling and learning how to use scrapy in practice part.3 and part.4

--- training/scrapyproject/ecommerce/ecommerce/spiders/gmarket_category_all.py
+++ training/scrapyproject/ecommerce/ecommerce/spiders/gmarket_category_all.py
@@ -15,5 +15,26 @@ class GmarketCategoryAllSpider(scrapy.Spider):
         category_names = response.css(
             'div.gbest-cate ul.by-group li a::text').getall()
         for index, category_link in enumerate(category_links):
-            print(
-                f'http://corners.gmarket.co.kr{category_link}', category_names[index])
+            yield scrapy.Request(url=f'http://corners.gmarket.co.kr{category_link}', callback=self.parse_maincategory, meta={'maincategory_name': category_names[index]})
+
+    def parse_maincategory(self, response):
+        print("parse_maincategory", response.meta['maincategory_name'])
+
+        best_items = response.css('div.best-list')
+        for index, item in enumerate(best_items[1].css('li')):
+            ranking = index + 1
+            title = item.css('a.itemname::text').get()
+            ori_price = item.css('div.o-price::text').get()
+            dis_price = item.css('div.s-price strong span span::text').get()
+            discount_percent = item.css('div.s-price em::text').get()
+
+            if ori_price == None:
+                ori_price = dis_price
+            ori_price = ori_price.replace(',', '').replace('원', '')
+            dis_price = dis_price.replace(',', '').replace('원', '')
+            if discount_percent == None:
+                discount_percent = 0
+            else:
+                discount_percent = discount_percent.replace('%', '')
+
+            print(ranking, title, ori_price, dis_price, discount_percent)
